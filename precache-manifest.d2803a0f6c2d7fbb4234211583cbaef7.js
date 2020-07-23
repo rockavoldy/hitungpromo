cf9b787b374cea52101e8c3b503fb1a80730b012
rockavoldy <48157179+rockavoldy@users.noreply.github.com>
2020-07-23 16:27:00 +0000

    Deploying to gh-pages from  @ d0d71298e86cbb05e8be92f54c232e9f76c5d6b7 🚀

---
+++ precache-manifest.d2803a0f6c2d7fbb4234211583cbaef7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e491fd2beba296446722",
+    "url": "/hitungpromo/css/app.e5f47e9b.css"
+  },
+  {
+    "revision": "2de6776021a7ab2cafc11bde8f3235b4",
+    "url": "/hitungpromo/index.html"
+  },
+  {
+    "revision": "e491fd2beba296446722",
+    "url": "/hitungpromo/js/app.b2f30758.js"
+  },
+  {
+    "revision": "210057faa9afe85827ba",
+    "url": "/hitungpromo/js/chunk-vendors.426acf7d.js"
+  },
+  {
+    "revision": "0997a65c7480958eecb6a6f47defd255",
+    "url": "/hitungpromo/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/hitungpromo/robots.txt"
+  }
+]);
