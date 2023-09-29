057eae8d2aa96744119fe576b1672db46d69972c
rockavoldy <akhmad0598@gmail.com>
2023-09-29 14:24:24 +0000

    Deploying to gh-pages from  @ b85e9eb7c6aadee1e2187ea127402c057ba0c9be 🚀

---
+++ precache-manifest.b78ee2e0ba302f2c90eae01b022eae43.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "853ff1b2e9967706f5ea",
+    "url": "/css/app.48d57d8d.css"
+  },
+  {
+    "revision": "d7484871dd5f690905c13bfb0e7e4d1e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "853ff1b2e9967706f5ea",
+    "url": "/js/app.96381026.js"
+  },
+  {
+    "revision": "ad148861b26b4d9008da",
+    "url": "/js/chunk-vendors.4b8979fa.js"
+  },
+  {
+    "revision": "0997a65c7480958eecb6a6f47defd255",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
